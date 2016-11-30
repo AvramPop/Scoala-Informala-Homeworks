b500eb3aeb98d40e2aa659fc9b0096d5f858207d
AvramPop <Dany4God@gmail.com>
2016-11-30 15:20:50 +0200

    Added javadoc, tests and improved logic.

---
+++ tema5/test/net/vompi/StringUtilityTest.java
@@ -0,0 +1,97 @@
+package net.vompi;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by dani on 11/30/16.
+ */
+public class StringUtilityTest {
+    @Test
+    public void stringWithWhitespacesHasThemRemoved(){
+        // given
+        String string = new String("Dani are multe mere!");
+        StringUtility stringUtility = new StringUtility();
+        // when
+        String correctedString = stringUtility.removeWhitespaces(string);
+        // then
+        String correctlyFormattedArray = new String("Daniaremultemere!");
+        Assert.assertEquals(correctedString, correctlyFormattedArray);
+    }
+
+    @Test
+    public void stringWithoutWhitespacesStaysUnmodified(){
+        // given
+        String string = new String("Daniaremultemere!");
+        StringUtility stringUtility = new StringUtility();
+        // when
+        String correctedString = stringUtility.removeWhitespaces(string);
+        // then
+        String correctlyFormattedArray = new String("Daniaremultemere!");
+        Assert.assertEquals(correctedString, correctlyFormattedArray);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void removeWhitespaces_whenEmptyStringUsed_ExceptionIsThrown(){
+        String string = new String("");
+        StringUtility stringUtility = new StringUtility();
+        stringUtility.removeWhitespaces(string);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void removeWhitespaces_whenNullStringUsed_ExceptionIsThrown(){
+        String string = null;
+        StringUtility stringUtility = new StringUtility();
+        stringUtility.removeWhitespaces(string);
+    }
+
+    @Test
+    public void stringWithDigitsAsWordsHasThemChanged(){
+        // given
+        String string = new String("I have four apples!");
+        StringUtility stringUtility = new StringUtility();
+        // when
+        String correctedString = stringUtility.convertNumbersInLiteralsFromStringToDigits(string);
+        // then
+        String correctlyFormattedArray = new String("I have 4 apples!");
+        Assert.assertEquals(correctedString, correctlyFormattedArray);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void replaceDigits_whenEmptyStringUsed_ExceptionIsThrown(){
+        String string = new String("");
+        StringUtility stringUtility = new StringUtility();
+        stringUtility.convertNumbersInLiteralsFromStringToDigits(string);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void replaceDigits_whenNullStringUsed_ExceptionIsThrown(){
+        String string = null;
+        StringUtility stringUtility = new StringUtility();
+        stringUtility.convertNumbersInLiteralsFromStringToDigits(string);
+    }
+
+    @Test
+    public void stringWithoutDigitsAsWordsRemainsUnchanged(){
+        // given
+        String string = new String("I have forty-nine apples!");
+        StringUtility stringUtility = new StringUtility();
+        // when
+        String correctedString = stringUtility.convertNumbersInLiteralsFromStringToDigits(string);
+        // then
+        String correctlyFormattedArray = new String("I have forty-nine apples!");
+        Assert.assertEquals(correctedString, correctlyFormattedArray);
+    }
+
+    @Test
+    public void stringWithAllWordsDigitsAsWordsHasThemChanged(){
+        // given
+        String string = new String("Five six SEVEN eiGHt !!");
+        StringUtility stringUtility = new StringUtility();
+        // when
+        String correctedString = stringUtility.convertNumbersInLiteralsFromStringToDigits(string);
+        // then
+        String correctlyFormattedArray = new String("5 6 7 8 !!");
+        Assert.assertEquals(correctedString, correctlyFormattedArray);
+    }
+}
